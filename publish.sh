6ba29e2b67b12548524894e1cfed06e1cbe6cb55
bigzhu <vermiliondun@gmail.com>
2016-10-05 09:58:59 +0800

    update sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,1 @@
-#! /bin/bash
-# sh ./change_name.sh
-# npm run build
-commit='git commit -am "'$1'"'
-echo $commit
-eval $commit
-# npm version patch
-# npm publish ./
-# git push
+../bz-demo/publish.sh
